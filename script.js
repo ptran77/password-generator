eabca55b18ba843432c41cf62f5919d59ee5802d
Peter Tran <pettran866@gmail.com>
2022-03-31 16:24:25 -0700

    adding password length prompt

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 // Assignment code here
 
+// function to get the length of the password
+var getPassLength = function () {
+  var passwordLength = window.prompt("Choose the length of your password within the range 8 to 128:");
+  passwordLength = parseInt(passwordLength);
+
+  if(passwordLength >= 8 && passwordLength <= 128){
+    return passwordLength;
+  }
+  else {
+    window.alert("You did not pick a valid option. Try again.");
+    return getPassLength();
+  }
+};
+
+var generatePassword = function () {
+  var passwordLength = getPassLength();
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
