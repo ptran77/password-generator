9432b93506cb4f82b37f5ccb9e339cbb3c5ae939
Peter Tran <pettran866@gmail.com>
2022-04-01 19:46:51 -0700

    remodify character prompts

--- script.js
+++ script.js
@@ -18,74 +18,14 @@ var getPassLength = function () {
   }
 };
 
-// Function to determine if user wants lowercase letters
-var includeLowerCase = function () {
-  var lowerCasePrompt = window.prompt("Include lowercase characters? Enter 1 for TRUE, 2 for FALSE.");
-  lowerCasePrompt = parseInt(lowerCasePrompt);
-  switch(lowerCasePrompt) {
-    case 1:
-      return true;
-    case 2:
-      return false;
-    default:
-      window.alert("You did not pick a valid option. Try again.");
-    return includeLowerCase();
-  }
-};
-
-// Function to determine if user wants uppercase letters
-var includeUpperCase = function () {
-  var upperCasePrompt = window.prompt("Include uppercase characters? Enter 1 for TRUE, 2 for FALSE.");
-  upperCasePrompt = parseInt(upperCasePrompt);
-  switch(upperCasePrompt) {
-    case 1:
-      return true;
-    case 2:
-      return false;
-    default:
-      window.alert("You did not pick a valid option. Try again.");
-    return includeUpperCase();
-  }
-};
-
-// Function to determine if user wants numeric characters
-var includeNumber = function () {
-  var numberPrompt = window.prompt("Include numeric characters? Enter 1 for TRUE, 2 for FALSE.");
-  numberPrompt = parseInt(numberPrompt);
-  switch(numberPrompt) {
-    case 1:
-      return true;
-    case 2:
-      return false;
-    default:
-      window.alert("You did not pick a valid option? Try again.");
-    return includeNumber();
-  }
-};
-
-// Function to determine if user wants special characters
-var includeSpecialChar = function () {
-  var specialCharPrompt = window.prompt("Include special characters? Enter 1 for TRUE, 2 for FALSE.");
-  specialCharPrompt = parseInt(specialCharPrompt);
-  switch(specialCharPrompt) {
-    case 1:
-      return true;
-    case 2:
-      return false;
-    default:
-      window.alert("You did not pick a valid option. Try again.");
-    return includeSpecialChar();
-  }
-};
-
 var generatePassword = function () {
   var passwordLength = getPassLength();
 
   while (true) {
-    var haveLowerCase = includeLowerCase();
-    var haveUpperCase = includeUpperCase();
-    var haveNumber = includeNumber();
-    var haveSpecialChar = includeSpecialChar();
+    var haveLowerCase = window.confirm("Include lowercase characters?");
+    var haveUpperCase = window.confirm("Include uppercase characrers?");
+    var haveNumber = window.confirm("Inclide numeric characters?");
+    var haveSpecialChar = window.confirm("Include special characters?");
     if (haveLowerCase === false && haveUpperCase === false && haveNumber === false && haveSpecialChar === false) {
       window.alert("There are no characters to use. Try Again.");
     }
